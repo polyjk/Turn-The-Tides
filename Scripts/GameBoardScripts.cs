dfd72754173e1fdb0929dfe81107ac53cf87f42f
polyjk <35551671+polyjk@users.noreply.github.com>
2020-01-19 14:08:07 -0600

    Add files via upload

---
+++ Scripts/GameBoardScripts.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameBoardScripts : MonoBehaviour
+{
+    public GameObject[] tiles = null;
+    public GameObject[] billboards = null;
+
+    private void OnTriggerEnter(Collider other) {
+     
+    }
+
+
+    public int getTotalTilesClaimed() {
+
+        int count = 0;
+        foreach (GameObject tile in tiles) {
+
+            if (GetComponentInChildren<BallDetector>(tile).playerClaim != 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int getP1TileTotal() {
+
+        int count = 0;
+        foreach (GameObject tile in tiles) {
+
+            if (GetComponentInChildren<BallDetector>(tile).playerClaim == 1) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int getP2TileTotal() {
+
+        int count = 0;
+        foreach (GameObject tile in tiles) {
+
+            if (GetComponentInChildren<BallDetector>(tile).playerClaim == 2) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int claimCt = 0, pOneCt = 0, pTwoCt = 0;
+        foreach (GameObject billboard in billboards) {
+            claimCt = getTotalTilesClaimed();
+            pOneCt = getP1TileTotal();
+            pTwoCt = getP2TileTotal();
+            billboard.GetComponent<TextMeshPro>().SetText("Claimed: {0}\n P1: {1}\n P2: {2}", claimCt, pOneCt, pTwoCt);
+        }
+    }
+}
---
+++ Scripts/RailMover.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RailMover : MonoBehaviour
+{
+    public Rail rail;
+
+    public float speed = 17.0f;
+    public bool isLooping;
+    //public float direction;
+
+    private int currentSeg;
+    private float transition;
+    private bool isCompleted;
+
+    private void Update() {
+        if (!rail)
+            return;
+        if (!isCompleted) {
+
+           // direction = Input.GetAxis("Horizontal");
+           if (Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow))
+               Play();
+           else if (Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow))
+               Play(false);
+        }
+
+    }
+
+    private void Play(bool direction = true) {
+
+        float m = (rail.nodes[currentSeg + 1].position - rail.nodes[currentSeg].position).magnitude;
+        float s = (Time.deltaTime * 1 / m) * speed;
+
+        transition += (direction) ? s : -s;
+
+        if (transition > 1) {
+            transition = 0;
+            currentSeg++;
+            if (currentSeg == rail.nodes.Length - 1) {
+                if (isLooping) {
+                    currentSeg = 0;
+                }
+            }
+        }
+        else if(transition < 0) {
+            transition = 1;
+            currentSeg--;
+            if (currentSeg == -1) {
+                if (isLooping)
+                    currentSeg = rail.nodes.Length - 2; 
+            }
+        }
+
+        transform.position = rail.LinearPosition(currentSeg, transition);
+        //transform.rotation = rail.Orientation(currentSeg, transition);
+    }
+}
